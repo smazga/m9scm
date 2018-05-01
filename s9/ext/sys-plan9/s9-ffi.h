7faad727e5300babe495c7d3b502ce1d77acb740
McKay Marston <1486270+smazga@users.noreply.github.com>
2018-05-01 16:56:22 -0600

    slowly morphing 9p into something useable

--- s9/ext/sys-plan9/s9-ffi.h
+++ s9/ext/sys-plan9/s9-ffi.h
@@ -7,3 +7,4 @@ long			int32_value(char *src, cell x);
 unsigned long		uint32_value(char *src, cell x);
 long long		int64_value(char *src, cell x);
 unsigned long long	uint64_value(char *src, cell x);
+ushort		ushort_value(char *src, cell x);
