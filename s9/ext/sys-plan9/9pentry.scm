3591b559484e6c5d7b3aa8bb5cb1ab4adaaa8714
McKay Marston <smazga@greymanlabs.com>
2018-10-08 12:47:21 -0600

    added a first pass at a generic 9p:entry class

---
+++ s9/ext/sys-plan9/9pentry.scm
@@ -0,0 +1,17 @@
+(load-from-library "s9sos.scm")
+
+(define-class 9p:entry ())
+(define-class 9p:dir (9p:entry))
+(define-class 9p:file (9p:entry))
+(define-generic 9p:read)
+
+(define-method (9p:read (f 9p:file) fc)
+	(let ((offset (fcall-u1 fc))
+				(contents (slot-ref f 'contents)))
+		(substring contents offset (string-length contents))))
+		
+(define-method (9p:read (d 9p:dir) fc)
+	(let ((offset (fcall-u1 fc))
+				(contents "bogusdirectory"))
+		(substring contents offset (string-length contents))))
+
