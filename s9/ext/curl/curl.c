b5caa4bdf2231bfacb8541ee6207a7e6350056c9
McKay Marston <smazga@greymanlabs.com>
2018-10-09 17:28:20 -0600

    added curl_getinfo...still feeling out the design of this and adding ugly hacks

--- s9/ext/curl/curl.c
+++ s9/ext/curl/curl.c
@@ -6,17 +6,18 @@
  * Hooks into the libcurl easy API
  */
 
-
-/* REMOVE ME:
- *  - make sure cleanup is called
- */
-
 #include "s9core.h"
 #include "s9import.h"
 #include "s9ext.h"
 
 #include <curl/curl.h>
-#include "const.h"
+#include "curl.h"
+
+/* total hack */
+#define GET_HANDLE { \
+	handle = easylist[integer_value(name, car(x))]; \
+	if (handle == NULL) \
+		return error("no curl handle", x); }
 
 static CURL *easylist[1024];
 static int easyidx = 0;
@@ -46,9 +47,10 @@ cell pp_easy_init(cell x) {
 
 cell pp_cleanup(cell x) {
 	CURL *handle;
+	char *name = "curl:cleanup";
 	int idx;
 
-	idx = integer_value("curl:cleanup", car(x));
+	idx = integer_value(name, car(x));
 	handle = easylist[idx];
 
 	if (handle != NULL) {
@@ -63,15 +65,10 @@ cell pp_setopt(cell x) {
 	CURL *handle;
 	CURLoption option;
 	CURLcode code;
-	int idx;
 	char name[] = "sys:setopt";
 
 	code = CURLE_OK;
-	idx = integer_value(name, car(x));
-	handle = easylist[idx];
-
-	if (handle == NULL)
-		return error("no curl handle", x);
+	GET_HANDLE;
 
 	option = find_magic_const(cdr(x));
 	if (_curl_is_long_option(option)) {
@@ -95,14 +92,83 @@ cell pp_setopt(cell x) {
 	return UNSPECIFIC;
 }
 
+#define CHECK(c) { if (c != CURLE_OK) goto infofail; }
+cell pp_getinfo(cell x) {
+	CURL *handle;
+	CURLINFO info;
+	CURLcode code = CURLE_OK;
+	char *name = "curl:getinfo";
+
+	GET_HANDLE;
+	info = find_magic_const(cdr(x));
+
+	if (_curl_is_long_info(info)) {
+		long result;
+		code = curl_easy_getinfo(handle, info, &result);
+		CHECK(code);
+		return make_integer(result);
+	}
+
+	if (_curl_is_string_info(info)) {
+		char *result;
+		code = curl_easy_getinfo(handle, info, &result);
+		CHECK(code);
+		return make_string(result, strlen(result));
+	}
+
+	if (_curl_is_slist_info(info)) {
+		cell n;
+		struct curl_slist *result = NULL;
+		int count = 0;
+
+		fprintf(stderr, "checking for cookies\n");
+		code = curl_easy_getinfo(handle, info, &result);
+		CHECK(code);
+
+		/* there must be a better way */
+		if (result) {
+			cell new;
+			struct curl_slist *each = result;
+
+			while(each) {
+				count++;
+				each = each->next;
+			}
+
+			fprintf(stderr, "%d COOKIES\n", count);
+			n = make_vector(count);
+			each = result;
+			count = 0;
+
+			save(n); /* why? - based on sys_convM2D */
+			while(each) {
+				new = make_string(each->data, strlen(each->data)); vector(n)[count++] = new;
+			}
+			unsave(1);
+			return n;
+		}
+		else
+			fprintf(stderr, "NO COOKIES?\n");
+	}
+
+	return UNSPECIFIC;
+
+infofail:
+	{
+		char err[16+CURL_ERROR_SIZE];
+		memset(err, 0, sizeof(err));
+		snprintf(err, sizeof(err)-1, "%s: %s", name, curl_easy_strerror(code));
+		return error(err, x);
+	}
+}
+#undef CHECK
+
 cell pp_perform(cell x) {
 	CURL *handle;
 	CURLcode code;
-	int idx;
 	char name[] = "sys:perform";
 
-	idx = integer_value(name, car(x));
-	handle = easylist[idx];
+	GET_HANDLE;
 
 	code = curl_easy_perform(handle);
 	if (code != CURLE_OK) {
@@ -115,12 +181,12 @@ cell pp_perform(cell x) {
 	return UNSPECIFIC;
 }
 
-
 S9_PRIM Curl_primitives[] = {
 	{ "curl:easy-init",	pp_easy_init,		0, 0, { ___,___,___ } },
 	{ "curl:setopt",	pp_setopt,		3, 3, { INT,STR,___ } },
 	{ "curl:perform",	pp_perform,		1, 1, { INT,___,___ } },
 	{ "curl:cleanup",	pp_cleanup,		1, 1, { INT,___,___ } },
+	{ "curl:getinfo",	pp_getinfo,		2, 2, { INT,STR,___ } },
 	{ "sys:magic-const",	pp_sys_magic_const,	1, 1, { STR,___,___ } },
 };
 
