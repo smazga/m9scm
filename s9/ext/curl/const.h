eb79085bf09f7a955eb535023b7dffba8b6079ac
McKay Marston <1486270+smazga@users.noreply.github.com>
2018-10-09 17:28:20 -0600

    added curl_getinfo...still feeling out the design of this and adding ugly hacks

--- s9/ext/curl/const.h
+++ s9/ext/curl/const.h
@@ -1,5 +1,3 @@
-/* ********************************************************* */
-/* This is the wrong place for this. unix/get_magic_value should be replaced with this */
 #define	K(x)	{#x, (int)x}
 struct Magic_const {
 	char*	name;
@@ -270,103 +268,68 @@ static Magic_const magic_const[] = {
 	K(CURLOPT_XFERINFODATA),
 	K(CURLOPT_XFERINFOFUNCTION),
 	K(CURLOPT_XOAUTH2_BEARER),
+	K(CURLINFO_ACTIVESOCKET),
+	K(CURLINFO_APPCONNECT_TIME),
+	/* K(CURLINFO_APPCONNECT_TIME_T), */
+	K(CURLINFO_CERTINFO),
+	K(CURLINFO_CONDITION_UNMET),
+	K(CURLINFO_CONNECT_TIME),
+	/* K(CURLINFO_CONNECT_TIME_T), */
+	K(CURLINFO_CONTENT_LENGTH_DOWNLOAD),
+	/* K(CURLINFO_CONTENT_LENGTH_DOWNLOAD_T), */
+	K(CURLINFO_CONTENT_LENGTH_UPLOAD),
+	/* K(CURLINFO_CONTENT_LENGTH_UPLOAD_T), */
+	K(CURLINFO_CONTENT_TYPE),
+	K(CURLINFO_COOKIELIST),
+	K(CURLINFO_EFFECTIVE_URL),
+	K(CURLINFO_FILETIME),
+	/* K(CURLINFO_FILETIME_T), */
+	K(CURLINFO_FTP_ENTRY_PATH),
+	K(CURLINFO_HEADER_SIZE),
+	K(CURLINFO_HTTPAUTH_AVAIL),
+	K(CURLINFO_HTTP_CONNECTCODE),
+	K(CURLINFO_HTTP_VERSION),
+	K(CURLINFO_LASTSOCKET),
+	K(CURLINFO_LOCAL_IP),
+	K(CURLINFO_LOCAL_PORT),
+	K(CURLINFO_NAMELOOKUP_TIME),
+	/* K(CURLINFO_NAMELOOKUP_TIME_T), */
+	K(CURLINFO_NUM_CONNECTS),
+	K(CURLINFO_OS_ERRNO),
+	K(CURLINFO_PRETRANSFER_TIME),
+	/* K(CURLINFO_PRETRANSFER_TIME_T), */
+	K(CURLINFO_PRIMARY_IP),
+	K(CURLINFO_PRIMARY_PORT),
+	K(CURLINFO_PRIVATE),
+	K(CURLINFO_PROTOCOL),
+	K(CURLINFO_PROXYAUTH_AVAIL),
+	K(CURLINFO_PROXY_SSL_VERIFYRESULT),
+	K(CURLINFO_REDIRECT_COUNT),
+	K(CURLINFO_REDIRECT_TIME),
+	/* K(CURLINFO_REDIRECT_TIME_T), */
+	K(CURLINFO_REDIRECT_URL),
+	K(CURLINFO_REQUEST_SIZE),
+	K(CURLINFO_RESPONSE_CODE),
+	K(CURLINFO_RTSP_CLIENT_CSEQ),
+	K(CURLINFO_RTSP_CSEQ_RECV),
+	K(CURLINFO_RTSP_SERVER_CSEQ),
+	K(CURLINFO_RTSP_SESSION_ID),
+	K(CURLINFO_SCHEME),
+	K(CURLINFO_SIZE_DOWNLOAD),
+	/* K(CURLINFO_SIZE_DOWNLOAD_T), */
+	K(CURLINFO_SIZE_UPLOAD),
+	/* K(CURLINFO_SIZE_UPLOAD_T), */
+	K(CURLINFO_SPEED_DOWNLOAD),
+	/* K(CURLINFO_SPEED_DOWNLOAD_T), */
+	K(CURLINFO_SPEED_UPLOAD),
+	/* K(CURLINFO_SPEED_UPLOAD_T), */
+	K(CURLINFO_SSL_ENGINES),
+	K(CURLINFO_SSL_VERIFYRESULT),
+	K(CURLINFO_STARTTRANSFER_TIME),
+	/* K(CURLINFO_STARTTRANSFER_TIME_T), */
+	K(CURLINFO_TLS_SESSION),
+	K(CURLINFO_TLS_SSL_PTR),
+	K(CURLINFO_TOTAL_TIME),
+	/* K(CURLINFO_TOTAL_TIME_T), */
 	{0,0}
 };
-
-int find_magic_const(cell x) {
-	char*		name = string(car(x));
-	Magic_const	*k;
-
-	for (k = magic_const; k->name; k++)
-		if (strcmp(k->name, name) == 0)
-			return k->value;
-	return -1;
-}
-
-cell pp_sys_magic_const(cell x) {
-	return make_integer(find_magic_const(x));
-}
-/* ********************************************************* */
-
-/* This is taken directly from the curl source...I guess this is one way to handle dynamic types in c */
-#define _curl_is_long_option(option)                                          \
-  (0 < (option) && (option) < CURLOPTTYPE_OBJECTPOINT)
-
-#define _curl_is_string_option(option)                                        \
-  ((option) == CURLOPT_ABSTRACT_UNIX_SOCKET ||                                \
-   (option) == CURLOPT_ACCEPT_ENCODING ||                                     \
-   (option) == CURLOPT_CAINFO ||                                              \
-   (option) == CURLOPT_CAPATH ||                                              \
-   (option) == CURLOPT_COOKIE ||                                              \
-   (option) == CURLOPT_COOKIEFILE ||                                          \
-   (option) == CURLOPT_COOKIEJAR ||                                           \
-   (option) == CURLOPT_COOKIELIST ||                                          \
-   (option) == CURLOPT_CRLFILE ||                                             \
-   (option) == CURLOPT_CUSTOMREQUEST ||                                       \
-   (option) == CURLOPT_DEFAULT_PROTOCOL ||                                    \
-   (option) == CURLOPT_DNS_INTERFACE ||                                       \
-   (option) == CURLOPT_DNS_LOCAL_IP4 ||                                       \
-   (option) == CURLOPT_DNS_LOCAL_IP6 ||                                       \
-   (option) == CURLOPT_DNS_SERVERS ||                                         \
-   (option) == CURLOPT_EGDSOCKET ||                                           \
-   (option) == CURLOPT_FTPPORT ||                                             \
-   (option) == CURLOPT_FTP_ACCOUNT ||                                         \
-   (option) == CURLOPT_FTP_ALTERNATIVE_TO_USER ||                             \
-   (option) == CURLOPT_INTERFACE ||                                           \
-   (option) == CURLOPT_ISSUERCERT ||                                          \
-   (option) == CURLOPT_KEYPASSWD ||                                           \
-   (option) == CURLOPT_KRBLEVEL ||                                            \
-   (option) == CURLOPT_LOGIN_OPTIONS ||                                       \
-   (option) == CURLOPT_MAIL_AUTH ||                                           \
-   (option) == CURLOPT_MAIL_FROM ||                                           \
-   (option) == CURLOPT_NETRC_FILE ||                                          \
-   (option) == CURLOPT_NOPROXY ||                                             \
-   (option) == CURLOPT_PASSWORD ||                                            \
-   (option) == CURLOPT_PINNEDPUBLICKEY ||                                     \
-   (option) == CURLOPT_PRE_PROXY ||                                           \
-   (option) == CURLOPT_PROXY ||                                               \
-   (option) == CURLOPT_PROXYPASSWORD ||                                       \
-   (option) == CURLOPT_PROXYUSERNAME ||                                       \
-   (option) == CURLOPT_PROXYUSERPWD ||                                        \
-   (option) == CURLOPT_PROXY_CAINFO ||                                        \
-   (option) == CURLOPT_PROXY_CAPATH ||                                        \
-   (option) == CURLOPT_PROXY_CRLFILE ||                                       \
-   (option) == CURLOPT_PROXY_KEYPASSWD ||                                     \
-   (option) == CURLOPT_PROXY_PINNEDPUBLICKEY ||                               \
-   (option) == CURLOPT_PROXY_SERVICE_NAME ||                                  \
-   (option) == CURLOPT_PROXY_SSLCERT ||                                       \
-   (option) == CURLOPT_PROXY_SSLCERTTYPE ||                                   \
-   (option) == CURLOPT_PROXY_SSLKEY ||                                        \
-   (option) == CURLOPT_PROXY_SSLKEYTYPE ||                                    \
-   (option) == CURLOPT_PROXY_SSL_CIPHER_LIST ||                               \
-   (option) == CURLOPT_PROXY_TLSAUTH_PASSWORD ||                              \
-   (option) == CURLOPT_PROXY_TLSAUTH_USERNAME ||                              \
-   (option) == CURLOPT_PROXY_TLSAUTH_TYPE ||                                  \
-   (option) == CURLOPT_RANDOM_FILE ||                                         \
-   (option) == CURLOPT_RANGE ||                                               \
-   (option) == CURLOPT_REFERER ||                                             \
-   (option) == CURLOPT_RTSP_SESSION_ID ||                                     \
-   (option) == CURLOPT_RTSP_STREAM_URI ||                                     \
-   (option) == CURLOPT_RTSP_TRANSPORT ||                                      \
-   (option) == CURLOPT_SERVICE_NAME ||                                        \
-   (option) == CURLOPT_SOCKS5_GSSAPI_SERVICE ||                               \
-   (option) == CURLOPT_SSH_HOST_PUBLIC_KEY_MD5 ||                             \
-   (option) == CURLOPT_SSH_KNOWNHOSTS ||                                      \
-   (option) == CURLOPT_SSH_PRIVATE_KEYFILE ||                                 \
-   (option) == CURLOPT_SSH_PUBLIC_KEYFILE ||                                  \
-   (option) == CURLOPT_SSLCERT ||                                             \
-   (option) == CURLOPT_SSLCERTTYPE ||                                         \
-   (option) == CURLOPT_SSLENGINE ||                                           \
-   (option) == CURLOPT_SSLKEY ||                                              \
-   (option) == CURLOPT_SSLKEYTYPE ||                                          \
-   (option) == CURLOPT_SSL_CIPHER_LIST ||                                     \
-   (option) == CURLOPT_TLSAUTH_PASSWORD ||                                    \
-   (option) == CURLOPT_TLSAUTH_TYPE ||                                        \
-   (option) == CURLOPT_TLSAUTH_USERNAME ||                                    \
-   (option) == CURLOPT_UNIX_SOCKET_PATH ||                                    \
-   (option) == CURLOPT_URL ||                                                 \
-   (option) == CURLOPT_USERAGENT ||                                           \
-   (option) == CURLOPT_USERNAME ||                                            \
-   (option) == CURLOPT_USERPWD ||                                             \
-   (option) == CURLOPT_XOAUTH2_BEARER ||                                      \
-   0)
