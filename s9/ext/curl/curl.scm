21fbc5ae0b82538de53ec5c401a43e5430a90a25
McKay Marston <smazga@greymanlabs.com>
2018-10-10 10:06:04 -0600

    improved handling of web stuff and renamed to http

--- s9/ext/curl/curl.scm
+++ s9/ext/curl/curl.scm
@@ -1,8 +1,25 @@
-(define (http-get url . options)
+(define (http:get url . options)
   (let ((easy (curl:easy-init)))
     (curl:setopt easy "CURLOPT_URL" url)
     (for-each (lambda (i)
-	       (curl:setopt easy (car i) (cdr i)))
-	     options)
+		(if (list? i)
+		 (for-each (lambda (j)
+			     (curl:setopt easy (car j) (cdr j))) i)
+		 (curl:setopt easy (car i) (cdr i))))
+	      options)
     (curl:perform easy)
     (curl:cleanup easy)))
+
+(define (http:enable_cookies)
+  '("CURLOPT_COOKIEFILE" . ""))
+
+(define (http:basic_auth)
+  (cons "CURLOPT_HTTPAUTH" (sys:magic-const "CURLAUTH_BASIC")))
+
+(define (http:user:pass user pass)
+  (cons "CURLOPT_USERPWD" (format #f "~A:~A" user pass)))
+
+(define (http:insecure)
+  (list
+   '("CURLOPT_SSL_VERIFYHOST" . 0)
+   '("CURLOPT_SSL_VERIFYPEER" . 0)))
