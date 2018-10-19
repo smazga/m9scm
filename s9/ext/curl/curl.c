d0a30deb6f2517cb0f704632ede60b310e0ff68f
McKay Marston <1486270+smazga@users.noreply.github.com>
2018-10-18 19:40:31 -0600

    curl improvements: * cookies are working * POST is not, for some reason

--- s9/ext/curl/curl.c
+++ s9/ext/curl/curl.c
@@ -17,22 +17,64 @@
 #define BUFFER_SIZE	8192
 
 /* total hack */
-#define GET_SESSION sessionlist[integer_value(name, car(x))]
-#define GET_HANDLE { \
-	handle = sessionlist[integer_value(name, car(x))]->handle; \
-	if (handle == NULL) \
-		return error("no curl handle", x); }
+#define SESSION sessionlist[integer_value(name, car(x))]
+
+#define CHECK_SESSION(s, c) \
+	if (s->handle == NULL) \
+		return error("no curl handle", c);
+
+#define CHECK_CODE(c) \
+	if (c != CURLE_OK) { \
+		char err[16+CURL_ERROR_SIZE]; \
+		memset(err, 0, sizeof(err)); \
+		snprintf(err, sizeof(err)-1, "%s: %s", name, curl_easy_strerror(code)); \
+		return error(err, x); \
+	}
 
 struct Session {
 	CURL *handle;
 	char *buffer;
-	int size;
+	size_t bufferlen;
+	struct curl_slist *cookies;
 };
 
 typedef struct Session Session;
 static Session *sessionlist[MAX_SESSIONS];
 static int sessionidx = 0;
 char error_buffer[CURL_ERROR_SIZE];
+
+cell slist2cell(struct curl_slist *slist) {
+	cell n, new;
+	struct curl_slist *each;
+	int count;
+
+	if (slist == NULL)
+		return make_vector(0);
+
+	each = slist;
+	count = 0;
+
+	/* ghetto, but we need a count for making a vector */
+	while (each) {
+		count++;
+		each = each->next;
+	}
+
+	n = make_vector(count);
+	count = 0;
+	each = slist;
+
+	save(n); /* why? - based on sys_convM2D */
+	while (each) {
+		new = make_string(each->data, strlen(each->data));
+		vector(n)[count++] = new;
+		each = each->next;
+	}
+	unsave(1);
+	curl_slist_free_all(each);
+
+	return n;
+}
 
 cell pp_easy_init(cell x) {
 	CURL *curl;
@@ -58,30 +100,32 @@ cell pp_easy_init(cell x) {
 }
 
 cell pp_cleanup(cell x) {
-	CURL *handle;
+	Session *session;
 	char *name = "curl:cleanup";
-	int idx;
 
-	idx = integer_value(name, car(x));
-	handle = sessionlist[idx]->handle;
+	session = SESSION;
 
-	if (handle != NULL) {
-		curl_easy_cleanup(handle);
-		free(sessionlist[idx]->buffer);
-		sessionlist[idx] = NULL;
+	if (session->cookies != NULL)
+		curl_slist_free_all(session->cookies);
+
+	if (session->handle != NULL) {
+		curl_easy_cleanup(session->handle);
+		free(session->buffer);
+		SESSION = NULL;
 	}
 
 	return UNSPECIFIC;
 }
 
 cell pp_setopt(cell x) {
-	CURL *handle;
+	Session *session;
 	CURLoption option;
 	CURLcode code;
 	char name[] = "sys:setopt";
 
 	code = CURLE_OK;
-	GET_HANDLE;
+	session = SESSION;
+	CHECK_SESSION(session, x);
 
 	option = find_magic_const(cdr(x));
 	fprintf(stderr, "option: %d: ", option);
@@ -89,13 +133,13 @@ cell pp_setopt(cell x) {
 		long parameter;
 		parameter = integer_value(name, caddr(x));
 		fprintf(stderr, "  %ld\n", parameter);
-		code = curl_easy_setopt(handle, option, parameter);
+		code = curl_easy_setopt(session->handle, option, parameter);
 	}
 	else if (_curl_is_string_option(option)) {
 		char *parameter;
 		parameter = string(caddr(x));
 		fprintf(stderr, "  %s\n", parameter);
-		code = curl_easy_setopt(handle, option, parameter);
+		code = curl_easy_setopt(session->handle, option, parameter);
 	}
 
 	if (code != CURLE_OK) {
@@ -108,89 +152,59 @@ cell pp_setopt(cell x) {
 	return UNSPECIFIC;
 }
 
-#define CHECK(c) { if (c != CURLE_OK) goto infofail; }
 cell pp_getinfo(cell x) {
-	CURL *handle;
+	Session *session;
 	CURLINFO info;
 	CURLcode code = CURLE_OK;
 	char *name = "curl:getinfo";
 
-	GET_HANDLE;
+	session = SESSION;
+	CHECK_SESSION(session, x);
+
 	info = find_magic_const(cdr(x));
 
 	if (_curl_is_long_info(info)) {
 		long result;
-		code = curl_easy_getinfo(handle, info, &result);
-		CHECK(code);
+		code = curl_easy_getinfo(session->handle, info, &result);
+		CHECK_CODE(code);
 		return make_integer(result);
 	}
 
 	if (_curl_is_string_info(info)) {
 		char *result;
-		code = curl_easy_getinfo(handle, info, &result);
-		CHECK(code);
+		code = curl_easy_getinfo(session->handle, info, &result);
+		CHECK_CODE(code);
 		return make_string(result, strlen(result));
 	}
 
 	if (_curl_is_slist_info(info)) {
 		cell n;
 		struct curl_slist *result = NULL;
-		int count = 0;
 
-		code = curl_easy_getinfo(handle, info, &result);
-		CHECK(code);
+		code = curl_easy_getinfo(session->handle, info, &result);
+		CHECK_CODE(code);
 
-		if (result) {
-			cell new;
-			struct curl_slist *each = result;
+		n = slist2cell(result);
+		curl_slist_free_all(result);
 
-			/* there must be a better way */
-			while(each) {
-				count++;
-				each = each->next;
-			}
-
-			n = make_vector(count);
-			each = result;
-			count = 0;
-
-			save(n); /* why? - based on sys_convM2D */
-			while(each) {
-				new = make_string(each->data, strlen(each->data));
-				vector(n)[count++] = new;
-				each = each->next;
-			}
-			unsave(1);
-			curl_slist_free_all(result);
-
-			return n;
-		}
+		return n;
 	}
 
-	return UNSPECIFIC;
-
-infofail:
-	{
-		char err[16+CURL_ERROR_SIZE];
-		memset(err, 0, sizeof(err));
-		snprintf(err, sizeof(err)-1, "%s: %s", name, curl_easy_strerror(code));
-		return error(err, x);
-	}
+	return NIL;
 }
-#undef CHECK
 
 static size_t write_callback(void *ptr, size_t size, size_t nmemb, void *data) {
-	size_t realsize;
 	Session *session;
+	size_t realsize;
 
 	realsize = size * nmemb;
 	session = (Session*)data;
-	session->buffer = realloc(session->buffer, session->size + realsize + 1);
+	session->buffer = realloc(session->buffer, session->bufferlen + realsize + 1);
 
 	if (session->buffer) {
-		memcpy(&(session->buffer[session->size]), ptr, realsize);
-		session->size += realsize;
-		session->buffer[session->size] = 0;
+		memcpy(&(session->buffer[session->bufferlen]), ptr, realsize);
+		session->bufferlen += realsize;
+		session->buffer[session->bufferlen] = 0;
 	}
 
 	return realsize;
@@ -202,10 +216,12 @@ cell pp_perform(cell x) {
 	char name[] = "sys:perform";
 	cell n;
 
-	session = GET_SESSION;
+	session = SESSION;
+	CHECK_SESSION(session, x);
 
 	curl_easy_setopt(session->handle, CURLOPT_WRITEFUNCTION, write_callback);
 	curl_easy_setopt(session->handle, CURLOPT_WRITEDATA, (void *)session);
+	curl_easy_setopt(session->handle, CURLOPT_COOKIEFILE, "");
 
 	code = curl_easy_perform(session->handle);
 	if (code != CURLE_OK) {
@@ -214,10 +230,47 @@ cell pp_perform(cell x) {
 		snprintf(err, sizeof(err)-1, "%s: %s", name, curl_easy_strerror(code));
 		return error(err, x);
 	}
+	curl_easy_getinfo(session->handle, CURLINFO_COOKIELIST, &(session->cookies));
 
-	n = make_string("", session->size);
-	memcpy(string(n), (char *)session->buffer, session->size);
+	n = make_string("", session->bufferlen);
+	memcpy(string(n), (char *)session->buffer, session->bufferlen);
 	return n;
+}
+
+cell pp_get_cookies(cell x) {
+	Session *session;
+	char *name = "curl:get-cookies";
+
+	session = SESSION;
+	CHECK_SESSION(session, x);
+
+	return slist2cell(session->cookies);
+}
+
+cell pp_set_cookies(cell x) {
+	Session *session;
+	CURLcode code;
+	char *name = "curl:set-cookies";
+	int count, i;
+	cell *v;
+
+	session = SESSION;
+	CHECK_SESSION(session, x);
+
+	if (!vector_p(cadr(x)))
+		return error("argument #2 not a vector", x);
+
+	v = vector(cadr(x));
+	count = vector_len(cadr(x));
+
+	fprintf(stderr, "cookie count: %d\n", count);
+	for (i = 0; i < count; i++) {
+		char *cookie = string(v[i]);
+		code = curl_easy_setopt(session->handle, CURLOPT_COOKIELIST, cookie);
+		CHECK_CODE(code);
+	}
+
+	return NIL;
 }
 
 S9_PRIM Curl_primitives[] = {
@@ -226,6 +279,8 @@ S9_PRIM Curl_primitives[] = {
 	{ "curl:perform",	pp_perform,		1, 1, { INT,___,___ } },
 	{ "curl:cleanup",	pp_cleanup,		1, 1, { INT,___,___ } },
 	{ "curl:getinfo",	pp_getinfo,		2, 2, { INT,STR,___ } },
+	{ "curl:get-cookies",   pp_get_cookies,         1, 1, { INT,___,___ } },
+	{ "curl:set-cookies",   pp_set_cookies,         2, 2, { INT,VEC,___ } },
 	{ "sys:magic-const",	pp_sys_magic_const,	1, 1, { STR,___,___ } },
 };
 
--- s9/lib/http.scm
+++ s9/lib/http.scm
@@ -1,7 +1,6 @@
 ;; Try to implement the following in both implementations so that http stuff can be platform agnostic
 
-;; http:enable_cookies		turn on cookie support
-;; http:basic_auth		enable http basi auth
+;; http:basic_auth		enable http basic auth
 ;; http:user:pass               take a user/pass combo and create a basic auth digest
 ;; http:insecure                turn off ssl safety checks
 
