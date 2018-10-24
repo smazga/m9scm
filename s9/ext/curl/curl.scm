dd2dd68d35a5ab221112cc06c25ac4da0c729883
McKay Marston <1486270+smazga@users.noreply.github.com>
2018-10-24 08:03:58 -0600

    added http:get-cookie to get a particular cookie

--- s9/ext/curl/curl.scm
+++ s9/ext/curl/curl.scm
@@ -1,3 +1,5 @@
+(load-from-library "regex.scm")
+
 (define (http:new-session url)
   (let ((easy (curl:easy-init)))
     (curl:setopt easy "CURLOPT_URL" url)
@@ -14,13 +16,16 @@
   (curl:perform easy))
 
 (define (apply-options easy options)
-  (if (null? options) #t
-      (begin
-	(curl:setopt easy (car options) (cadr options))
-	(apply-options easy (cddr options)))))
+  (if (null? options)
+      #t
+      (begin (curl:setopt easy (car options) (cadr options))
+             (apply-options easy (cddr options)))))
 
 (define (http:basic_auth easy)
-  (curl:setopt easy "CURLOPT_HTTPAUTH" (sys:magic-const "CURLAUTH_BASIC")))
+  (curl:setopt
+    easy
+    "CURLOPT_HTTPAUTH"
+    (sys:magic-const "CURLAUTH_BASIC")))
 
 (define (http:user:pass easy user pass)
   (curl:setopt easy "CURLOPT_USERPWD" (format #f "~A:~A" user pass)))
@@ -31,3 +36,16 @@
 
 (define (http:get-cookies easy)
   (curl:get-cookies easy))
+
+(define (match-regex rgx items)
+  (if (null? items)
+      '()
+      (let ((match (re-match rgx (car items))))
+        (if match
+            (car items)
+            (match-regex rgx (cdr items))))))
+
+(define (http:get-cookie easy regex)
+  (let ((rgx (re-comp regex))
+        (cookies (vector->list (curl:get-cookies easy))))
+    (match-regex rgx cookies)))
